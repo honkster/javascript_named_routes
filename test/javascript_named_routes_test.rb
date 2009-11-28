178615b1bc564960925932e2c640ac5dffe4b16f
Frederick Polgardy <fred@polgardy.com>
2009-11-27 21:26:01 -0600

    test case conversion
    finally finished a test case conversion i started months ago to dynamically calculate Rails and JS route URLs and compare them for equality

--- test/javascript_named_routes_test.rb
+++ test/javascript_named_routes_test.rb
@@ -6,25 +6,30 @@ require 'action_controller/test_process'
 
 # initialization attempts to delete cached js under RAILS_ROOT
 RAILS_ROOT = '/tmp'
+require 'javascript_named_routes'
 
 # initialize the plugin
 require File.dirname(__FILE__) + '/../init'
 
 module JavascriptNamedRoutes
   class RoutesControllerTest < ActionController::TestCase
-    def setup
-      ActionController::Routing::Routes.draw do |map|
-        map.resources :users do |users|
-          users.resources :accounts
-        end
-        map.javascript_named_routes
+    
+    # a simple two-level resource controller
+    
+    ActionController::Routing::Routes.draw do |map|
+      map.resources :users do |users|
+        users.resources :accounts
       end
+      
+      map.javascript_named_routes
     end
     
     def test_action_success
       get :index
       assert_response :success
     end
+    
+    # test that the following named routes generated the correct route definition in routes.js
     
     [
       '"usersPath":route(["/","users"],[])',
@@ -35,7 +40,7 @@ module JavascriptNamedRoutes
       '"editUserAccountPath":route(["/","users","/",_,"/","accounts","/",_,"/","edit"],["user_id","id"])'
     ].each do |expected|
       route_name = expected.match(/^"(\w+)"/).captures.first
-      define_method("test_js_generation_for_#{route_name}") do
+      define_method("test_js_generation_#{route_name}") do
         help_test_js_generation(expected)
       end
     end
@@ -48,24 +53,44 @@ module JavascriptNamedRoutes
     
     # should be true if you have a Mac and JavaScript OSA installed
     if system('osascript -l JavaScript -e 0 2> /dev/null')
-      {
-        "Routes.usersPath()" => "/users",
-        "Routes.usersPath({format: 'xml'})" => "/users.xml",
-        "Routes.userPath(123)" => "/users/123",
-        "Routes.userPath(123, {foo: 42})" => "/users/123?foo=42",
-        "Routes.userPath({id: 123})" => "/users/123",
-        "Routes.editUserPath(123)" => "/users/123/edit",
-        "Routes.editUserPath({id: 123})" => "/users/123/edit",
-        "Routes.editUserPath({id: 123, foo: 42, bar: 43})" => "/users/123/edit?foo=42&bar=43",
-        "Routes.userAccountsPath(123)" => "/users/123/accounts",
-        "Routes.userAccountPath(123, 456)" => "/users/123/accounts/456",
-        "Routes.userAccountPath({id: 456, user_id: 123})" => "/users/123/accounts/456",
-        "Routes.editUserAccountPath(123, 456)" => "/users/123/accounts/456/edit",
-        "Routes.editUserAccountPath(123, 456, {foo: 42})" => "/users/123/accounts/456/edit?foo=42"
-      }.each_pair do |expr, expected|
-        route_name = expr.match(/^Routes\.(\w+)/).captures.first
-        define_method("test_js_route_for_#{route_name}") do
-          help_test_js_route(expr, expected)
+      
+      # test that the following named routes evaluate to the correct URL on the JS side
+      
+      # For each named route w/ args in the list, convert it into the corresponding JS expr -
+      # e.g. [:users_path, 123, {:foo => 42}] becomes Routes.usersPath(123,{"foo":42}).  Then
+      # generate a test method which invokes the route helper - users_path(123, :foo => 42) -
+      # on the Rails side, and verifies that the results are equal.
+      
+      [
+        [:users_path],
+        [:users_path, {:format => 'xml'}],
+        [:user_path, 123],
+        [:user_path, 123, {:foo => 42}],
+        [:user_path, {:id => 123}],
+        [:edit_user_path, 123],
+        [:edit_user_path, {:id => 123}],
+        [:user_accounts_path, 123],
+        [:user_account_path, 123, 456],
+        [:user_account_path, {:id => 123, :user_id => 456}],
+        [:edit_user_account_path, 123, 456],
+        [:edit_user_account_path, 123, 456, {:foo => 42}]
+      ].each do |route_args|
+        route_name = route_args.shift
+        
+        # generate a test name from the route name and args 
+        test_name = "test_js_route_#{route_name}"
+        test_name += "_#{route_args}" if !route_args.empty?
+        
+        # camelize the route name and convert any args to JSON
+        # to generate the expr that will be evaluated by OSA
+        js_name = route_name.to_s.camelize(:lower)
+        js_args = route_args.collect(&:to_json)
+        js_expr = "Routes.#{js_name}(#{js_args.join(',')})"
+        
+        define_method(test_name) do
+          # get the expected value by invoking the Rails route helper
+          expected = send(route_name, *route_args)
+          help_test_js_route(js_expr, expected)
         end
       end
     else
@@ -75,6 +100,7 @@ module JavascriptNamedRoutes
     def help_test_js_route(expr, expected)
       get :index
       Open3.popen3('osascript', '-l', 'JavaScript') do |input, output|
+        # eval the routes.js file followed by the expression we want to evaluate
         input << @response.body << expr
         input.close
         assert_equal expected, output.readline.chomp
